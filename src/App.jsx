00fdd90a74140f680fa7f6ed1134944ed152027c
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-03-16 07:33:13 +0600

    Added recipy title section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Banner from './Banner/Banner'
 import Nav from './Navbar/Nav'
+import RecipyTitle from './RecipyTitle/RecipyTitle'
 
 function App() {
 
   return (
     <>
       <Nav></Nav>
-      <Banner></Banner> 
+      <Banner></Banner>
+      <RecipyTitle></RecipyTitle> 
     </>
   )
 }
---
+++ src/RecipyTitle/RecipyTitle.jsx
@@ -0,0 +1,18 @@
+const RecipyTitle = () => {
+  return (
+    <div className="w-11/12 mx-auto text-center mt-24">
+      <div className="w-4/5 mx-auto">
+        <h2 className="text-[#150B2B] text-4xl font-semibold font-Lexend mb-6">
+          Our Recipes
+        </h2>
+        <p className="text-[#150B2B99] text-lg font-medium">
+          Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus
+          vulputate netus pharetra rhoncus. Eget urna volutpat curabitur
+          elementum mauris aenean neque.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RecipyTitle;
